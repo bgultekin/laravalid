030f179b47e4585e3392ea0460289c3aed2084e0
Jannis Pohl <j.pohl@qn-solutions.de>
2015-06-11 10:50:23 +0200

    Fixes attribute names not being translated for non-default messages (laravel4)

--- src/Bllim/Laravalid/converter/JqueryValidation/Message.php
+++ src/Bllim/Laravalid/converter/JqueryValidation/Message.php
@@ -1,63 +1,64 @@
 <?php namespace Bllim\Laravalid\Converter\JqueryValidation;
 
 use Lang;
+use Bllim\Laravalid\Helper;
 
 class Message extends \Bllim\Laravalid\Converter\Base\Message {
 
-	public function ip($parsedRule, $attribute, $type) 
+	public function ip($parsedRule, $attribute, $type)
 	{
-		$message = Lang::get('validation.'.$parsedRule['name'], ['attribute' => $attribute]);
+		$message = Lang::get('validation.'.$parsedRule['name'], ['attribute' => Helper::getAttributeName($attribute)]);
 		return ['data-msg-ipv4' => $message];
 	}
-	
-	public function alpha($parsedRule, $attribute, $type) 
+
+	public function alpha($parsedRule, $attribute, $type)
 	{
-		$message = Lang::get('validation.'.$parsedRule['name'], ['attribute' => $attribute]);
+		$message = Lang::get('validation.'.$parsedRule['name'], ['attribute' => Helper::getAttributeName($attribute)]);
 		return ['data-msg-regex' => $message];
 	}
-	
-	public function alphanum($parsedRule, $attribute, $type) 
+
+	public function alphanum($parsedRule, $attribute, $type)
 	{
-		$message = Lang::get('validation.'.$parsedRule['name'], ['attribute' => $attribute]);
+		$message = Lang::get('validation.'.$parsedRule['name'], ['attribute' => Helper::getAttributeName($attribute)]);
 		return ['data-msg-regex' => $message];
 	}
-	
+
 	public function max($parsedRule, $attribute, $type)
 	{
-		$message = Lang::get('validation.'.$parsedRule['name'].'.'.$type, ['attribute' => $attribute, 'max' => $parsedRule['parameters'][0]]);
+		$message = Lang::get('validation.'.$parsedRule['name'].'.'.$type, ['attribute' => Helper::getAttributeName($attribute), 'max' => $parsedRule['parameters'][0]]);
 		switch ($type) {
 			case 'numeric':
 				return ['data-msg-max' => $message];
 				break;
-			
+
 			default:
 				return ['data-msg-maxlength' => $message];
 				break;
 		}
 	}
-	
+
 	public function min($parsedRule, $attribute, $type)
 	{
-		$message = Lang::get('validation.'.$parsedRule['name'].'.'.$type, ['attribute' => $attribute, 'min' => $parsedRule['parameters'][0]]);
+		$message = Lang::get('validation.'.$parsedRule['name'].'.'.$type, ['attribute' => Helper::getAttributeName($attribute), 'min' => $parsedRule['parameters'][0]]);
 		switch ($type) {
 			case 'numeric':
 				return ['data-msg-min' => $message];
 				break;
-			
+
 			default:
 				return ['data-msg-minlength' => $message];
 				break;
 		}
 	}
-	
+
 	public function between($parsedRule, $attribute, $type)
 	{
-		$message = Lang::get('validation.'.$parsedRule['name'].'.'.$type, ['attribute' => $attribute, 'min' => $parsedRule['parameters'][0], 'max' => $parsedRule['parameters'][1]]);
+		$message = Lang::get('validation.'.$parsedRule['name'].'.'.$type, ['attribute' => Helper::getAttributeName($attribute), 'min' => $parsedRule['parameters'][0], 'max' => $parsedRule['parameters'][1]]);
 		switch ($type) {
 			case 'numeric':
 				return ['data-msg-range' => $message];
 				break;
-			
+
 			default:
 				return ['data-msg-minlength' => $message, 'data-msg-maxlength' => $message];
 				break;
