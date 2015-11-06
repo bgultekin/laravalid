b33f6fce1fca9074607c532244d370538990f283
Kaido <kaido@smartsoft.ee>
2015-11-06 16:20:38 -0500

    changed remote route to use controller.route cache does not work with closures

--- src/Bllim/Laravalid/LaravalidServiceProvider.php
+++ src/Bllim/Laravalid/LaravalidServiceProvider.php
@@ -29,10 +29,7 @@ class LaravalidServiceProvider extends ServiceProvider {
 		$routeName = \Config::get('laravalid.route');
 
 		// remote validations
-		\Route::any($routeName.'/{rule}', function($rule){
-			return $this->app['laravalid']->converter()->route()->convert($rule, \Input::all());
-		});
-
+		\Route::any($routeName.'/{rule}', '\Bllim\Laravalid\RuleController@getIndex');
 	}
 
 	/**
