0d3ded53ee6ff3beedf980cacdde970f0f0000fe
Kaido <kaido@smartsoft.ee>
2017-01-25 16:30:06 +0200

    Replaced: Input with Request

--- src/Bllim/Laravalid/RuleController.php
+++ src/Bllim/Laravalid/RuleController.php
@@ -3,11 +3,12 @@ namespace Bllim\Laravalid;
 
 use Illuminate\Routing\Controller as BaseController;
 use Bllim\Laravalid\Converter\JqueryValidation\Converter;
+use Illuminate\Http\Request;
 
 class RuleController extends BaseController
 {
-	public function getIndex($rule)
+	public function getIndex(Request $request, $rule)
 	{
-		return (new Converter())->route()->convert($rule, \Input::all());
+		return (new Converter())->route()->convert($rule, $request->all());
 	}
 }
