141647e43eda3963124629030253d867bbd2f2b4
Thach Nguyen <nthachus@gmail.com>
2018-02-25 03:07:32 +0700

    Title-case `converter` folder to make Travis unit-test work correctly

--- src/Bllim/Laravalid/converter/JqueryValidation/Converter.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Bllim\Laravalid\Converter\JqueryValidation;
-
-class Converter extends \Bllim\Laravalid\Converter\Base\Converter {
-
-}
--- src/Bllim/Laravalid/converter/JqueryValidation/Route.php
+++
@@ -1,5 +0,0 @@
-<?php namespace Bllim\Laravalid\Converter\JqueryValidation;
-
-class Route extends \Bllim\Laravalid\Converter\Base\Route {
-
-}
